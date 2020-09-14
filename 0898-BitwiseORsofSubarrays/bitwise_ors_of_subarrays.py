c17829ce10928d62eed19e0c11274af3049f7b35
Quoc Anh Trinh <trinhquocanh94@gmail.com>
2020-09-14 21:04:47 +0200

    Solve bitwise operation

---
+++ 0898-BitwiseORsofSubarrays/bitwise_ors_of_subarrays.py
@@ -0,0 +1,10 @@
+from typing import List
+
+
+class Solution:
+    def subarrayBitwiseORs(self, A: List[int]) -> int:
+        cur, res = set(), set()
+        for i in A:
+            cur = {i | j for j in cur} | {i}
+            res |= cur
+        return len(res)
