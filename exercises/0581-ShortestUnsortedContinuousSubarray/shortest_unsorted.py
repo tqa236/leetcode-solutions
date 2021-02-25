62141341e5c1c20ac314cfe1184199bf9d52d2db
Trinh Quoc Anh <trinhquocanh94@gmail.com>
2021-02-25 11:35:27 +0100

    Update 581

--- exercises/0581-ShortestUnsortedContinuousSubarray/shortest_unsorted.py
+++ exercises/0581-ShortestUnsortedContinuousSubarray/shortest_unsorted.py
@@ -4,21 +4,29 @@ import bisect
 
 class Solution:
     def findUnsortedSubarray(self, nums: List[int]) -> int:
+        lowest = nums[0]
+        highest = None
+        curr_highest = None
         prev = None
         start = False
         start_index = None
         end_index = None
         for i, curr in enumerate(nums):
             if prev is None:
-                lowest = prev = curr_highest = highest = curr
+                prev = curr
+                curr_highest = curr
+                highest = curr
                 continue
             if not start and curr < prev:
                 start = True
                 start_index = i - 1
                 lowest = curr
+
             if curr < prev:
+                end_index = i
                 highest = curr_highest
-            lowest = min(curr, lowest)
+            if curr < lowest:
+                lowest = curr
             curr_highest = max(curr_highest, curr)
             if curr < highest:
                 end_index = i
@@ -27,6 +35,20 @@ class Solution:
             return 0
         start_index = bisect.bisect_right(nums[:start_index], lowest)
         return end_index - start_index + 1
+
+    def findUnsortedSubarray2(self, nums: List[int]) -> int:
+        start = -1
+        end = -2
+        min_val = nums[-1]
+        max_val = nums[0]
+        for i in range(1, len(nums)):
+            max_val = max(max_val, nums[i])
+            min_val = min(min_val, nums[-i - 1])
+            if nums[i] < max_val:
+                end = i
+            if nums[-i - 1] > min_val:
+                start = len(nums) - i - 1
+        return end - start + 1
 
     def findUnsortedSubarrayNaive(self, nums: List[int]) -> int:
         sorted_nums = sorted(nums)
